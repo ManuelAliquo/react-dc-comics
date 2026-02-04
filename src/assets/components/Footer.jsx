3fbeaa03478e3a31f9a3949b81689cd5a033f6d8
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-04 21:10:00 +0100

    added style

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="page-footer text-white">
-      <section className="footer-nav d-flex align-items-center py-4">
-        <nav className="container-custom d-flex justify-content-around align-items-center">
+      <section className="footer-nav d-flex align-items-center">
+        <nav className="container-custom d-flex justify-content-evenly align-items-center">
           <a href="#">
             <img src="src/assets/img/buy-comics-digital-comics.png" alt="Digital Comics" />
             <span>DIGITAL COMICS</span>
@@ -25,40 +25,40 @@ export default function Footer() {
           </a>
         </nav>
       </section>
-      <section className="footer-links pt-4 pb-5">
+      <section className="footer-links">
         <div className="container-custom d-flex gap-4">
           <div className="list-container ">
             <h6>DC COMICS</h6>
             <ul>
               <li>
-                <a href="#">link</a>
+                <a href="#">Characters</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Comics</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Movies</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">TV</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Games</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Videos</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">News</a>
               </li>
             </ul>
             <h6 className="mt-3">SHOP</h6>
             <ul>
               <li>
-                <a href="#">link</a>
+                <a href="#">Shop DC</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Shop DC Collectibles</a>
               </li>
             </ul>
           </div>
@@ -66,37 +66,37 @@ export default function Footer() {
             <h6>DC</h6>
             <ul>
               <li>
-                <a href="#">link</a>
+                <a href="#">Terms Of Use</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Privacy policy (New)</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Ad Choices</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Advertising</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Jobs</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Subscriptions</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Talent Workshops</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">CPSC Certificates</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Ratings</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Shop Help</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">Contact Us</a>
               </li>
             </ul>
           </div>
@@ -104,22 +104,27 @@ export default function Footer() {
             <h6>SITES</h6>
             <ul>
               <li>
-                <a href="#">link</a>
+                <a href="#">DC</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">MAD Magazine</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">DC Kids</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">DC Universe</a>
               </li>
               <li>
-                <a href="#">link</a>
+                <a href="#">DC Power Visa</a>
               </li>
             </ul>
           </div>
+          <img
+            className="background-logo"
+            src="src/assets/img/dc-logo-bg.png"
+            alt="Background Logo"
+          />
         </div>
       </section>
       <section className="social-section py-4">
@@ -129,11 +134,21 @@ export default function Footer() {
           </a>
           <div className="d-flex align-items-center gap-3">
             <span>FOLLOW US</span>
-            <img src="src\assets\img\footer-facebook.png" alt="Facebook" />
-            <img src="src\assets\img\footer-twitter.png" alt="Twitter" />
-            <img src="src\assets\img\footer-youtube.png" alt="YouTube" />
-            <img src="src\assets\img\footer-pinterest.png" alt="Pinterest" />
-            <img src="src\assets\img\footer-periscope.png" alt="Periscope" />
+            <a href="#">
+              <img src="src\assets\img\footer-facebook.png" alt="Facebook" />
+            </a>
+            <a href="#">
+              <img src="src\assets\img\footer-twitter.png" alt="Twitter" />
+            </a>
+            <a href="#">
+              <img src="src\assets\img\footer-youtube.png" alt="YouTube" />
+            </a>
+            <a href="#">
+              <img src="src\assets\img\footer-pinterest.png" alt="Pinterest" />
+            </a>
+            <a href="#">
+              <img src="src\assets\img\footer-periscope.png" alt="Periscope" />
+            </a>
           </div>
         </div>
       </section>
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -2,41 +2,41 @@ export default function Header() {
   return (
     <header className="page-header">
       <div className="container-custom d-flex justify-content-between h-100">
-        <img src="src/assets/img/dc-logo.png" alt="Logo" />
-        <nav className="d-flex align-items-center">
-          <ul className="d-flex gap-3 list-unstyled mb-0">
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-            <li>
-              <a href="#">Link</a>
-            </li>
-          </ul>
-        </nav>
+        <a href="#">
+          <img src="src/assets/img/dc-logo.png" alt="Logo" />
+        </a>
+        <ul className="d-flex align-items-center gap-4 mb-0">
+          <li>
+            <a href="#">CHARACTERS</a>
+          </li>
+          <li className="active">
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
       </div>
     </header>
   );
