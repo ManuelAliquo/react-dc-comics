a2739cdd7a9dedc43553f2112bdce096309a1dec
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-04 17:11:24 +0100

    added basic header + main placeholder

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,3 +1,39 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="d-flex justify-content-between mt-2">
+      <img src="src/assets/img/dc-logo.png" alt="Logo" />
+      <ul className="d-flex align-items-center gap-3 list-unstyled mb-0">
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+        <li>
+          <a href="#">Link</a>
+        </li>
+      </ul>
+    </header>
+  );
 }
--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,3 +1,6 @@
 export default function Main() {
-  return <main></main>;
+  const mainPlaceholder = "--> Content goes here <--";
+  return (
+    <main className="d-flex align-items-center text-white fw-bold fs-4">{mainPlaceholder}</main>
+  );
 }
